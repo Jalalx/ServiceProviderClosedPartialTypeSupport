b2df7ec05bfb7358112b69bf39299f7e06459af4
Jalal Amini Robati <j.amini@alibaba.ir>
2020-03-02 22:02:26 +0330

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+Sample repo for issue here: [#19507](https://github.com/dotnet/aspnetcore/issues/19507)
+ASP.NET Core 2.2 can resolve registered partially-closed generic types, but ASP.NET Core 3.1 throws System.ArgumentException
---
+++ ServiceResolveSample3.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ServiceResolveSample2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var provider = new ServiceCollection()
+                .AddSingleton(typeof(Service<>), typeof(Service2<>))
+                .BuildServiceProvider();
+
+            var service2 = provider.GetService(typeof(Service<Status>));
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public enum Status { Pending, Done }
+
+    public class Service<TEnum> where TEnum : Enum
+    {
+        public TEnum Status { get; set; }
+    }
+
+    public class Service2<TEnum> where TEnum : Enum
+    {
+        public TEnum Status { get; set; }
+    }
+
+}
